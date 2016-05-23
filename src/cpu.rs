eb54d6ece84b2260fd34e1e74738892bcbbf845e
tompko <tompko@gmail.com>
2016-05-23 21:00:59 +0100

    Read in rom and create cpu

---
+++ src/cpu.rs
@@ -0,0 +1,11 @@
+pub struct Cpu {
+}
+
+impl Cpu {
+    pub fn new(rom: Vec<u8>) -> Cpu {
+        return Cpu{}
+    }
+
+    pub fn Run(&self) {
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#[macro_use]
+extern crate clap;
+
+use std::fs::File;
+use std::io::Read;
+use clap::{Arg, App};
+
+mod cpu;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("CHIP 8 Emulator")
+                        .version(crate_version!())
+                        .author("tompko  <tompko@gmail.com>")
+                        .about("Emulates the CHIP8 language")
+                        .arg(Arg::with_name("INPUT")
+                            .help("Sets the input file to use")
+                            .required(true)
+                            .index(1))
+                        .get_matches();
+
+    let input_file = matches.value_of("INPUT").unwrap();
+    let rom = read_rom(input_file);
+
+    let cpu = cpu::Cpu::new(rom);
+
+    cpu.Run();
+}
+
+fn read_rom(filename: &str) -> Vec<u8> {
+    let mut buffer = Vec::new();
+
+    match File::open(filename) {
+        Ok(ref mut file) => {
+            file.read_to_end(&mut buffer).unwrap();
+        },
+        Err(err) => {
+            println!("chip8: cannot open '{}': {}", filename, err);
+            std::process::exit(-1);
+        }
+    }
+
+
+    return buffer
 }
