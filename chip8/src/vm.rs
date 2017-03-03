ba7c1e39bc4704b9ad411d92ce3081c25e3a5980
Chris Tompkinson <chris.tompkinson@xtxmarkets.com>
2017-03-03 13:02:22 +0000

    Run at max of 500 instructions per second

--- chip8/src/vm.rs
+++ chip8/src/vm.rs
@@ -1,5 +1,9 @@
 use super::interconnect::Interconnect;
 use super::cpu::Cpu;
+use super::time::{Duration, SteadyTime};
+use std::thread;
+
+const NSECS_PER_FRAME: i64 = 1000000000 / 500;
 
 pub struct VM {
     inter: Interconnect,
@@ -21,6 +25,9 @@ impl VM {
     }
 
     pub fn run(&mut self) {
+        let mut start_time = SteadyTime::now();
+        let mut nsecs_elapsed = 0;
+
         loop {
             self.cpu.step(&mut self.inter);
 
@@ -29,6 +36,25 @@ impl VM {
 
             if self.inter.input.quit {
                 break;
+            }
+
+            let mut now = SteadyTime::now();
+            let mut duration_elapsed = now - start_time;
+            nsecs_elapsed += duration_elapsed.num_nanoseconds().expect("Loop took too long");
+            start_time = now;
+
+            let nsecs_to_sleep = NSECS_PER_FRAME - nsecs_elapsed;
+            if nsecs_to_sleep > 0 {
+                let sleep_duration = Duration::nanoseconds(nsecs_to_sleep);
+                thread::sleep(sleep_duration.to_std().unwrap());
+            }
+
+            now = SteadyTime::now();
+            duration_elapsed = now - start_time;
+            nsecs_elapsed += duration_elapsed.num_nanoseconds().expect("Loop took too long");
+            while nsecs_elapsed > NSECS_PER_FRAME {
+                // Ignore fully missed frames
+                nsecs_elapsed -= NSECS_PER_FRAME;
             }
         }
     }
